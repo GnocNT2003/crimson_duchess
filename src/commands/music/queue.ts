c6b0528c2b2e33406f2d391870c2256562e1643b
Nguyen Thanh Dat <ntdatwows2003@gmail.com>
2026-06-10 10:24:06 +0700

    Refactor - combine queue and download commands - Queue have options to choose between temp (previous queue behavior) and download (previous download behavior) - Remove download command

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -3,7 +3,7 @@ import type { ChatInputCommandInteraction } from "discord.js";
 import type Command from "../../types/commandTypes.js";
 import { createLogger } from "../../tools/logging.js";
 import { extractYoutubeUrl } from "../../tools/youtubeHandler.js";
-import { getTempDownloadDir } from "../../tools/filePathResolver.js";
+import { getTempDownloadDir, getMusicDownloadsDir } from "../../tools/filePathResolver.js";
 import { YoutubeUrlType } from "../../types/youtubeUrlTypes.js";
 import { queueAndDownloadMusic } from "../../tools/queueHandler.js";
 
@@ -16,16 +16,28 @@ const queueCommand: Command = {
         .addStringOption((option) => 
             option
                 .setName('url')
-                .setDescription('The URL of the YouTube video to queue')
+                .setDescription('The URL of the YouTube video to queue and download')
                 .setRequired(true),
+        )
+        .addStringOption((option) => 
+            option
+                .setName('option')
+                .setDescription('Download options for audio, \'temp\' for temporary store, \'download\' for long term store')
+                .setChoices(
+                    [
+                        {name: 'temp', value: 'temp'},
+                        {name: 'download', value: 'download'},
+                    ]
+                )
         ) as SlashCommandBuilder,
 
     async execute(interaction: ChatInputCommandInteraction) {
         let url = interaction.options.getString('url', true);
-        const tempDownloadDir = getTempDownloadDir();
+        const downloadOption = interaction.options.getString('option', true) || 'temp';
+        const downloadDir = downloadOption === 'temp'? getTempDownloadDir() : getMusicDownloadsDir();
             
         logger.sep();
-        logger.log('START QUEUE');
+        logger.log(`START QUEUE ${downloadOption.toUpperCase()}`);
 
         try {
             url = extractYoutubeUrl(url, YoutubeUrlType.Video); 
@@ -46,7 +58,7 @@ const queueCommand: Command = {
                 return;
             }
     
-            await queueAndDownloadMusic(url, interaction.guild, tempDownloadDir, logger);
+            await queueAndDownloadMusic(url, interaction.guild, downloadDir, logger);
             await interaction.editReply(`The audio for URL: \`${url}\` has been added to the queue.`);
         } catch (error) {
             logger.log(`Error while trying to play music: ${String(error)}`);
